cdb387079b434fc55fa831e4132807276205ee5c
FrancisOwusu <osei1257@gmail.com>
2024-09-22 19:46:43 +0000

    add crud to contacts

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,48 +1,56 @@
-import { Link, Outlet } from "react-router-dom";
-
-export default function Root() {
+import {
+    Outlet,
+    Link,
+    useLoaderData,Form
+  } from "react-router-dom";
+  import { getContacts ,createContact} from "../contacts";
+  
+  /* other code */
+  export async function action() {
+    const contact = await createContact();
+    return { contact };
+  }
+  
+  export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+  }
+  export default function Root() {
+    const { contacts } = useLoaderData();
+    //useLoaderData();
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div>
+          {/* other code */}
+  
           <nav>
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite && <span>★</span>}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
-        </div>
-        <div id="detail">
-            <Outlet />
+  
+          {/* other code */}
         </div>
       </>
     );
   }
-  
