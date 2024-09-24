0c869f201ccb92cf5608241658860590ba3decb8
FrancisOwusu <osei1257@gmail.com>
2024-09-24 11:18:01 +0000

    add contact.js destroy.jsx,edit.jsx,index.jsx in mian.jsx

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,9 +3,90 @@ import { getContact } from "../contacts";
 
 
 export async function loader({ params }) {
-    const contact = await getContact(params.contactId);
-    return { contact };
-  }
+  const contact = await getContact(params.contactId);
+  return { contact };
+}
+// export default function Contact() {
+//   const contact = {
+//     first: "Your",
+//     last: "Name",
+//     avatar: "https://robohash.org/you.png?size=200x200",
+//     twitter: "your_handle",
+//     notes: "Some notes",
+//     favorite: true,
+//   };
+
+//   return (
+//     <div id="contact">
+//       <div>
+//         <img
+//           key={contact.avatar}
+//           src={
+//             contact.avatar ||
+//             `https://robohash.org/${contact.id}.png?size=200x200`
+//           }
+//         />
+//       </div>
+
+//       <div>
+//         <h1>
+//           {contact.first || contact.last ? (
+//             <>
+//               {contact.first} {contact.last}
+//             </>
+//           ) : (
+//             <i>No Name</i>
+//           )}{" "}
+//           <Favorite contact={contact} />
+//         </h1>
+
+//         {contact.twitter && (
+//           <p>
+//             <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+//               {contact.twitter}
+//             </a>
+//           </p>
+//         )}
+
+//         {contact.notes && <p>{contact.notes}</p>}
+
+//         <div>
+//           <Form action="edit">
+//             <button type="submit">Edit</button>
+//           </Form>
+//           <Form
+//             method="post"
+//             action="destroy"
+//             onSubmit={(event) => {
+//               if (!confirm("Please confirm you want to delete this record.")) {
+//                 event.preventDefault();
+//               }
+//             }}
+//           >
+//             <button type="submit">Delete</button>
+//           </Form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Favorite({ contact }) {
+//   const favorite = contact.favorite;
+//   return (
+//     <Form method="post">
+//       <button
+//         name="favorite"
+//         value={favorite ? "false" : "true"}
+//         aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+//       >
+//         {favorite ? "★" : "☆"}
+//       </button>
+//     </Form>
+//   );
+// }
+
+
 export default function Contact() {
   const contact = {
     first: "Your",
@@ -42,7 +123,10 @@ export default function Contact() {
 
         {contact.twitter && (
           <p>
-            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+            <a
+              target="_blank"
+              href={`https://twitter.com/${contact.twitter}`}
+            >
               {contact.twitter}
             </a>
           </p>
@@ -58,7 +142,11 @@ export default function Contact() {
             method="post"
             action="destroy"
             onSubmit={(event) => {
-              if (!confirm("Please confirm you want to delete this record.")) {
+              if (
+                !confirm(
+                  "Please confirm you want to delete this record."
+                )
+              ) {
                 event.preventDefault();
               }
             }}
@@ -78,8 +166,14 @@ function Favorite({ contact }) {
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
-        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
-      >{favorite ? "★" : "☆"}</button>
+        aria-label={
+          favorite
+            ? "Remove from favorites"
+            : "Add to favorites"
+        }
+      >
+        {favorite ? "★" : "☆"}
+      </button>
     </Form>
   );
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,56 +1,91 @@
 import {
-    Outlet,
-    Link,
-    useLoaderData,Form
-  } from "react-router-dom";
-  import { getContacts ,createContact} from "../contacts";
-  
-  /* other code */
-  export async function action() {
-    const contact = await createContact();
-    return { contact };
-  }
-  
-  export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
-  }
-  export default function Root() {
-    const { contacts } = useLoaderData();
-    //useLoaderData();
-    return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          {/* other code */}
-  
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite && <span>★</span>}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-  
-          {/* other code */}
+  Outlet,
+  NavLink,
+  Link,
+  useLoaderData,
+  Form,
+  redirect,
+  useNavigation
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+/* other code */
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+export default function Root() {
+  const { contacts } = useLoaderData();
+  //useLoaderData();
+  const navigation = useNavigation();
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        {/* other code */}
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
         </div>
-      </>
-    );
-  }
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+
+        {/* other code */}
+      </div>
+      <div
+        id="detail"
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
+        <Outlet />
+      </div>
+    </>
+  );
+}
